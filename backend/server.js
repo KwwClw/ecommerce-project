c4c76351ce84d75a57721459f860f7dff117b5d8
Kaweewat Chaloemwong <109239731+KwwClw@users.noreply.github.com>
2025-03-09 21:22:26 +0700

    update 9 mar

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/userRoutes")
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(express.json()); // ให้ Express อ่าน JSON ได้
@@ -20,12 +20,16 @@ app.get("/", (req, res) => {
   res.send("API is running... 5M");
 });
 
+app.get("/api/hello", (req, res) => {
+  res.status(200).json({ message: "Hello from the server!" });
+});
+
 // app.get('/api/products', (req, res) => {
 //   // Fetch products from database
 //   res.json({ products: [] });
 // });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000; // กำหนดค่า default สำหรับ PORT
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
