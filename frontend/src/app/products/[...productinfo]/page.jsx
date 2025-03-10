ff7562c1720bfb43f2f6d5c6ec8d53ba4ca56582
Kaweewat Chaloemwong <109239731+KwwClw@users.noreply.github.com>
2025-03-11 02:13:30 +0700

    Update page.jsx

--- frontend/src/app/products/[...productinfo]/page.jsx
+++ frontend/src/app/products/[...productinfo]/page.jsx
@@ -1,27 +1,17 @@
-// export default async function ProductPage({ params }) {
-//   console.log("Log:", params);  // ดูว่า params เป็นอย่างไร
+export default async function ProductPage({ params }) {
+  console.log("Log:", params);  // ดูว่า params เป็นอย่างไร
 
-//   const { productinfo } = await params;  // ดึงค่าจาก catch-all parameter
-//   const id = productinfo[0];   // กำหนดให้ id เป็นค่าแรกจาก productinfo
-//   const name = productinfo[1]; // กำหนดให้ name เป็นค่าที่สองจาก productinfo
+  const { productinfo } = await params;  // ดึงค่าจาก catch-all parameter
+  const id = productinfo[0];   // กำหนดให้ id เป็นค่าแรกจาก productinfo
+  const name = productinfo[1]; // กำหนดให้ name เป็นค่าที่สองจาก productinfo
 
-//   return (
-//     <div>
-//       <h1>Product Info</h1>
-//       {/* แสดงผลข้อมูลที่ได้จาก URL */}
-//       <p>Product ID: {id}</p>
-//       <p>Product Name: {name}</p>
-//       <p>Number of path segments: {productinfo.length}</p>
-//     </div>
-//   );
-// }
-
-import React from 'react'
-
-function page() {
   return (
-    <div>test page</div>
-  )
+    <div>
+      <h1>Product Info</h1>
+      {/* แสดงผลข้อมูลที่ได้จาก URL */}
+      <p>Product ID: {id}</p>
+      <p>Product Name: {name}</p>
+      <p>Number of path segments: {productinfo.length}</p>
+    </div>
+  );
 }
-
-export default page
