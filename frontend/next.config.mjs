570b08082e0cfe49d1957395af8b2cede214d00f
Kaweewat Chaloemwong <109239731+KwwClw@users.noreply.github.com>
2025-03-11 01:33:55 +0700

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // ลบ basePath ออกไปถ้าไม่ใช้
+  };
+  
+  export default nextConfig;
+  
