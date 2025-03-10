d9fd93853c8191b1fee8119e4fae27ff6a39115e
Kaweewat Chaloemwong <109239731+KwwClw@users.noreply.github.com>
2025-03-11 02:44:39 +0700

    update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-      return {
-        beforeFiles: [
-          {
-            source: '/products/:id/:name',  // เปลี่ยนเส้นทางที่คุณต้องการ
-            destination: '/products/:id/:name/index'  // เส้นทางใหม่ที่ Next.js จะเปลี่ยนไป
-          }
-        ]
-      }
-    }
-  }
-  
-  export default nextConfig;
-  
+    // ไม่ต้องใส่ basePath ถ้าแอปไม่ได้ถูก deploy ใน sub-path
+};
+
+export default nextConfig;
--- frontend/src/app/products/[...productinfo]/page.jsx
+++ frontend/src/app/products/[...productinfo]/page.jsx
@@ -4,6 +4,7 @@ export default async function ProductPage({ params }) {
   const { productinfo } = await params;  // ดึงค่าจาก catch-all parameter
   const id = productinfo[0];   // กำหนดให้ id เป็นค่าแรกจาก productinfo
   const name = productinfo[1]; // กำหนดให้ name เป็นค่าที่สองจาก productinfo
+  console.log(productinfo)
 
   return (
     <div>
