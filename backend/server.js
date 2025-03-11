de13428f2a3c47f2f3ee124a87b0a908ff2fec80
Kaweewat Chaloemwong <109239731+KwwClw@users.noreply.github.com>
2025-03-11 21:37:06 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,20 @@ const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+const productInsert = require("./routes/productInsert");
 
 const app = express();
 app.use(express.json()); // ให้ Express อ่าน JSON ได้
 app.use(cookieParser()); // อ่านค่าจาก Cookie
 app.use(cors()); // อนุญาตให้ frontend เรียก API
 
-connectDB();
+connectDB(); // เชื่อมต่อฐานข้อมูล
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use(productRoutes);
+app.use(productInsert);
 
 app.get("/api/index", (req, res) => {
   res.send("API is running...");
@@ -28,18 +32,6 @@ app.get("/api/index", (req, res) => {
 //   // Fetch products from database
 //   res.json({ products: [] });
 // });
-
-app.get("/api/products", (req, res) => {
-  const products = [
-    { id: 1, name: "Jordan 1 Retro Low OG SP Travis Scott Canary (Women's)", price: 11000, image: "/images/jordan-1-retro-low-og-sp-travis-scott-canary-w-1.webp" },
-    { id: 2, name: "Jordan 1 Retro Low OG SP Travis Scott Olive (Women's)", price: 28000, image: "/images/jordan-1-retro-low-og-sp-travis-scott-olive--w--1.webp" },
-    { id: 3, name: "Jordan 1 Retro Low OG SP Travis Scott Velvet Brown", price: 13000, image: "/images/nike-jordan-1-retro-low-og-sp-travis-scott-velvet-brown-1.webp" },
-    { id: 4, name: "Jordan 1 Retro Low OG SP Travis Scott Black Phantom", price: 17000, image: "/images/jordan-1-retro-low-og-sp-travis-scott-black-phantom-1.webp" },
-    { id: 5, name: "Jordan 1 Retro Low OG SP Fragment x Travis Scott", price: 74000, image: "/images/jordan-1-low-fragment-design-x-travis-scott-1.webp" },
-    { id: 6, name: "Jordan 1 Retro Low OG SP Travis Scott Mocha", price: 28000, image: "/images/air-jordan-1-retro-low-travis-scott-1.webp" },
-  ];
-  res.json(products);
-});
 
 const port = process.env.PORT || 5000; // กำหนดค่า default สำหรับ PORT
 app.listen(port, () => {
