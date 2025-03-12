5a5f99e37af3a44d4fa67515a852066d98eb2865
Kaweewat Chaloemwong <109239731+KwwClw@users.noreply.github.com>
2025-03-12 18:40:06 +0700

    Update page.jsx

--- frontend/src/app/products/[id]/page.jsx
+++ frontend/src/app/products/[id]/page.jsx
@@ -20,33 +20,33 @@ export async function generateStaticParams() {
   return result;
 }
 
-// export default async function ProductPage({ params }) {
-//   const res = await fetch(`${URL}/api/products/${params.id}`);
-//   const product = await res.json();
-//   // console.log(`id: ${params.id}`);
+export default async function ProductPage({ params }) {
+  const res = await fetch(`${URL}/api/products/${params.id}`);
+  const product = await res.json();
+  // console.log(`id: ${params.id}`);
 
-//   // ตรวจสอบว่ามีข้อมูลจาก API
-//   if (!product) {
-//     return <div>Product not found</div>;
-//   }
+  // ตรวจสอบว่ามีข้อมูลจาก API
+  if (!product) {
+    return <div>Product not found</div>;
+  }
 
-//   // เนื่องจากราคามีการใช้ $numberDecimal ให้แปลงเป็นค่า number ด้วย parseFloat
-//   const price = product.price?.$numberDecimal ? parseFloat(product.price.$numberDecimal) : product.price;
+  // เนื่องจากราคามีการใช้ $numberDecimal ให้แปลงเป็นค่า number ด้วย parseFloat
+  const price = product.price?.$numberDecimal ? parseFloat(product.price.$numberDecimal) : product.price;
 
-//   return (
-//     <div>
-//       <h1>{product.name}</h1>
-//       <p>ราคา: {price} บาท</p>
-//       <img src={product.image} alt={product.name} width={300} />
-//       <p>description: {product.description}</p>
-//     </div>
-//   );
-// }
-
-export default function PagProductPage({ params }) {
   return (
     <div>
-      <h1>My Post: {params.id}</h1>
+      <h1>{product.name}</h1>
+      <p>ราคา: {price} บาท</p>
+      <img src={product.image} alt={product.name} width={300} />
+      <p>description: {product.description}</p>
     </div>
   );
 }
+
+// export default function PagProductPage({ params }) {
+//   return (
+//     <div>
+//       <h1>My Post: {params.id}</h1>
+//     </div>
+//   );
+// }
