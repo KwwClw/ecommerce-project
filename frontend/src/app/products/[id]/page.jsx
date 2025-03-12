cb0e9437bab711401c52dbe332deb6eafde234c2
Kaweewat Chaloemwong <109239731+KwwClw@users.noreply.github.com>
2025-03-12 18:37:22 +0700

    test params

--- frontend/src/app/products/[id]/page.jsx
+++ frontend/src/app/products/[id]/page.jsx
@@ -15,28 +15,38 @@ export async function generateStaticParams() {
   const productIds = productsData.products.map((product) => product._id);
   const result = productIds.map((id) => ({ id }));
 
+  console.log(result); // log ผลลัพธ์ในรูปแบบที่ต้องการ
+
   return result;
 }
 
-export default async function ProductPage({ params }) {
-  const res = await fetch(`${URL}/api/products/${params.id}`);
-  const product = await res.json();
-  // console.log(`id: ${params.id}`);
+// export default async function ProductPage({ params }) {
+//   const res = await fetch(`${URL}/api/products/${params.id}`);
+//   const product = await res.json();
+//   // console.log(`id: ${params.id}`);
 
-  // ตรวจสอบว่ามีข้อมูลจาก API
-  if (!product) {
-    return <div>Product not found</div>;
-  }
+//   // ตรวจสอบว่ามีข้อมูลจาก API
+//   if (!product) {
+//     return <div>Product not found</div>;
+//   }
 
-  // เนื่องจากราคามีการใช้ $numberDecimal ให้แปลงเป็นค่า number ด้วย parseFloat
-  const price = product.price?.$numberDecimal ? parseFloat(product.price.$numberDecimal) : product.price;
+//   // เนื่องจากราคามีการใช้ $numberDecimal ให้แปลงเป็นค่า number ด้วย parseFloat
+//   const price = product.price?.$numberDecimal ? parseFloat(product.price.$numberDecimal) : product.price;
 
+//   return (
+//     <div>
+//       <h1>{product.name}</h1>
+//       <p>ราคา: {price} บาท</p>
+//       <img src={product.image} alt={product.name} width={300} />
+//       <p>description: {product.description}</p>
+//     </div>
+//   );
+// }
+
+export default function PagProductPage({ params }) {
   return (
     <div>
-      <h1>{product.name}</h1>
-      <p>ราคา: {price} บาท</p>
-      <img src={product.image} alt={product.name} width={300} />
-      <p>description: {product.description}</p>
+      <h1>My Post: {params.id}</h1>
     </div>
   );
 }
