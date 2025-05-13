5dfa213e2a786c2812e0528ecf9afc8240026cb4
Kaweewat Chaloemwong <109239731+KwwClw@users.noreply.github.com>
2025-05-13 18:50:12 +0700

    update Endpoints auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { registerUser, loginUser, getUserData, logoutUser } = require("../controllers/userController");
-
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.get("/me", getUserData);
-router.post("/logout", logoutUser);
+const { register, login, currentUser } = require("../controllers/auth")
+
+router.post("/register", register)
+router.post("/login", login)
+router.post("/current-user", currentUser)
+router.post("/current-admin", currentUser)
 
 module.exports = router;
