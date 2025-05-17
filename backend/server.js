beb26999b19f16c1cc59ee7897af2cbccf700b59
Kaweewat Chaloemwong <109239731+KwwClw@users.noreply.github.com>
2025-05-17 15:56:12 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,27 +2,47 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const connectDB = require("./config/db");
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/userRoutes");
-const productRoutes = require("./routes/productRoutes");
-const productInsert = require("./routes/productInsert");
+const morgan = require("morgan");
+const { readdirSync } = require("fs");
 
 const app = express();
+
+app.use(morgan("dev")); // ใช้ morgan สำหรับ log การเรียก API
 app.use(express.json()); // ให้ Express อ่าน JSON ได้
 app.use(cookieParser()); // อ่านค่าจาก Cookie
 app.use(cors()); // อนุญาตให้ frontend เรียก API
 
-connectDB(); // เชื่อมต่อฐานข้อมูล
+// console.log(readdirSync('./routes'));
 
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use(productRoutes);
-app.use(productInsert);
+// readdirSync("./routes").map((item, index, arr) => console.log(item));
 
-app.get("/api/index", (req, res) => {
-  res.send("API is running...");
+// readdirSync("./routes").map((item, index, arr) => {
+//   app.use("/api", require("./routes/" + item))
+//   console.log(`app.use("/api", require("./routes/${item}")`)
+// });
+
+readdirSync("./routes").forEach((item, index) => {
+  const path = `./routes/${item}`;
+  app.use("/api", require(path));
+  // console.log(`index: ${index} Loaded: ${path}`);
 });
+
+// readdirSync("./routes").map((item,index,arr) =>
+//   console.log(`app.use("/api", require("./routes/${item}")`)
+// );
+
+// app.use("/api", require("./routes/category"));
+
+// app.use("/api", categoryRoutes);
+// app.use("/api", authRoutes);
+
+// app.use("/api/users", userRoutes);
+// app.use(productRoutes);
+// app.use(productInsert);
+
+// app.get("/api/index", (req, res) => {
+//   res.send("API is running...");
+// });
 
 // app.get("/api/hello", (req, res) => {
 //   res.status(200).json({ message: "Hello from the server!" });
@@ -33,7 +53,7 @@ app.get("/api/index", (req, res) => {
 //   res.json({ products: [] });
 // });
 
-const port = process.env.PORT || 5000; // กำหนดค่า default สำหรับ PORT
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`✔ Server is running on port ${port}`);
 });
