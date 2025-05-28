f464660199189ed5e39c0c2ce6da4fd0cb664282
Kaweewat Chaloemwong <109239731+KwwClw@users.noreply.github.com>
2025-05-28 21:56:13 +0700

    Update product.js

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,13 +1,14 @@
 const express = require("express")
 const router = express.Router()
 
-const { create, list, update, remove, listby, searchFilters } = require("../controllers/product")
+const { create, list, read, update, remove, listby, searchFilters } = require("../controllers/product")
 
 // @ENDPOINT: /api/product
 router.post("/product", create)
 router.get("/products/:count", list)
-router.put("/product/:id/:name", update)
-router.delete("/product/:id/:name", remove)
+router.get("/product/:id", read)
+router.put("/product/:id", update)
+router.delete("/product/:id", remove)
 router.post("/productby", listby)
 router.post("/search/filters", searchFilters)
 
